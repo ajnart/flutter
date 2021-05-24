503f851cadd9bf812976182f668ad1b1e47f8bfb
ajnart <thomas.camlong@epitech.eu>
2021-05-24 19:18:37 +0200

    Added a basic question on the main screen

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  var questionIndex = 0;
+  var questions = [
+    'Do you like apples ? 🍎',
+    'Do you like skiing ? ⛷',
+    'What\'s your favorite animal ? 🐕',
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,9 +16,16 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text('Appbar'),
         ),
-        body: Column(children: <Widget>[
-          ElevatedButton(onPressed: null, child: Text('Say hello'), autofocus: true,)
-        ],),
+        body: Column(
+          children: <Widget>[
+            Text(questions.elementAt(0)),
+            ElevatedButton(
+              onPressed: () => {},
+              child: Text('Say hello'),
+              autofocus: true,
+            )
+          ],
+        ),
       ),
     );
   }
