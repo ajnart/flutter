e6c7889389b7a72f3bc7bb2cbaba9f2dd4a12d97
ajnart <thomas.camlong@epitech.eu>
2021-05-25 22:21:38 +0200

    Rename tab in web

--- linktree/lib/main.dart
+++ linktree/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ajnart\'s links',
+      title: 'Ajnart\'s LinkTree 🎄',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
