018cc523bb2148e3866d5fc7c99333ba609c6d02
ajnart <thomas.camlong@epitech.eu>
2021-05-27 23:27:09 +0200

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "32655b3f4a6e131e9843d37a454b26bc",
-"/": "32655b3f4a6e131e9843d37a454b26bc",
+"index.html": "b3fbbaa2b9000e108e833e63fb2c0968",
+"/": "b3fbbaa2b9000e108e833e63fb2c0968",
 "main.dart.js": "539f9fb46762b64d9dd071d98c38a846",
 "manifest.json": "c3c3ad886c823c74eadde8df8d722317",
 "version.json": "fdf7900a50a737ea88d14ce118d2e5e4"
