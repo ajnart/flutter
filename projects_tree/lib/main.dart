16741eea8f31a3e355b8d207526c2ccc9389a1a1
ajnart <thomas.camlong@epitech.eu>
2021-05-27 23:43:03 +0200

    Update main links

--- projects_tree/lib/main.dart
+++ projects_tree/lib/main.dart
@@ -88,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Padding(
                       padding: EdgeInsets.all(8),
                       child: SocialCard(
-                          url: '/projects/linktree',
+                          url: './linktree/',
                           asset: './assets/linktree.png',
                           label: "Link Tree",
                           description:
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Padding(
                       padding: EdgeInsets.all(8),
                       child: SocialCard(
-                          url: '/projects/managment',
+                          url: './managment/',
                           asset: 'assets/managment.png',
                           label: "Managment mockup",
                           description:
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Padding(
                       padding: EdgeInsets.all(8),
                       child: SocialCard(
-                        url: '/projects/quizz',
+                        url: './quizz/',
                         description:
                             "My first project! A simple quizz app with a darkmode switch that decides how much you are worth to society",
                         label: "Quizz",
