8dc6ae80cfa90e9c1c435fc31249dc6b70aee737
ajnart <thomas.camlong@epitech.eu>
2021-05-28 13:34:22 +0200

    Update style of the main widgets

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -50,11 +50,30 @@ class MyHomePage extends StatelessWidget {
                 return Card(
                     child: Row(
                   children: <Widget>[
-                    Container(child: Text(tx.ammount.toString())),
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                      child: Text(
+                        tx.ammount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title),
-                        Text(tx.date.toString())
+                        Text(
+                          tx.title,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(tx.date.toString(),
+                            style: TextStyle(color: Colors.grey))
                       ],
                     )
                   ],
