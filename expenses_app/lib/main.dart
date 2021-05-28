8a24d5a0e88513672373b019fcdbf406acad0f3c
ajnart <thomas.camlong@epitech.eu>
2021-05-29 00:25:03 +0200

    Update widgets styling

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -26,23 +26,25 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 150,
-              child: Card(
-                color: Colors.green,
-                elevation: 5,
-                child: Text("CHARTER"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 150,
+                child: Card(
+                  color: Colors.green,
+                  elevation: 5,
+                  child: Text("CHARTER"),
+                ),
               ),
-            ),
-            UserTransaction(),
-          ],
+              UserTransaction(),
+            ],
+          ),
         ),
       ),
     );
--- expenses_app/lib/widgets/transaction.dart
+++ expenses_app/lib/widgets/transaction.dart
@@ -7,45 +7,49 @@ class Transactions extends StatelessWidget {
   Transactions(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                child: Text(
-                  "${tx.ammount}€",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, idx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: Text(
+                    "${transactions[idx].ammount}€",
                     style: TextStyle(
-                      fontSize: 17,
                       fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
                   ),
-                  Text(
-                    DateFormat('EEEE d H:m').format(tx.date),
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[idx].title,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('EEEE d H:m').format(tx.date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: this.transactions.length,
+      ),
     );
   }
 }
