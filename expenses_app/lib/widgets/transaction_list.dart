106a7431c05beb258bdce405c59f6b6d523b5e5a
ajnart <thomas.camlong@epitech.eu>
2021-05-30 21:49:10 +0200

    Switched back to a mediaquerry

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 460,
+      height: MediaQuery.of(context).size.height / 1.6,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
