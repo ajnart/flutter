dc1b1077afe7954490085d3b6428898227a6b782
ajnart <thomas.camlong@epitech.eu>
2021-05-30 21:14:04 +0200

    Remove a SingleChildScrollView

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -10,57 +10,55 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        height: 300,
-        child: transactions.isEmpty
-            ? Column(
-                children: <Widget>[
-                  Text(
-                    'No transactions added yet!',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                      width: 300,
-                      child: Image.asset(
-                        'assets/images/empty.png',
-                        fit: BoxFit.fill,
-                      )),
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: FittedBox(
-                            child: Text("${transactions[index].amount}€"),
-                          ),
+    return Container(
+      height: 300,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    width: 300,
+                    child: Image.asset(
+                      'assets/images/empty.png',
+                      fit: BoxFit.fill,
+                    )),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: FittedBox(
+                          child: Text("${transactions[index].amount}€"),
                         ),
                       ),
-                      title: Text(transactions[index].title, style: Theme.of(context).textTheme.headline6),
-                      subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () => _deletetransaction(transactions[index].id),
-                      ),
                     ),
-                  );
-                },
-                itemCount: transactions.length,
-              ),
-      ),
+                    title: Text(transactions[index].title, style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => _deletetransaction(transactions[index].id),
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
