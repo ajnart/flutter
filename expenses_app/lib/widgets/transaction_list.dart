ff77b166fd19d030c915fe76f1b1c2f47b3644ee
ajnart <thomas.camlong@epitech.eu>
2021-05-30 21:19:23 +0200

    Make the transaction list as big as it can be

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: MediaQuery.of(context).size.height,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
