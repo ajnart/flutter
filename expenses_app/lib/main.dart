28a21cae6526fbb42451aca7daf728de18e52548
ajnart <thomas.camlong@epitech.eu>
2021-05-30 19:43:57 +0200

    Update title

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -103,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses App'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
