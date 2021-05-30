09373c8aae5d70e32545033d8844f42a9ea03e57
ajnart <thomas.camlong@epitech.eu>
2021-05-30 19:25:58 +0200

    Update styling of chart bar

--- expenses_app/lib/widgets/chart.dart
+++ expenses_app/lib/widgets/chart.dart
@@ -1,23 +1,39 @@
 import 'package:expenses_app/models/transaction.dart';
+import 'package:expenses_app/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class _Transaction {
+  final String day;
+  final double amount;
+  _Transaction(this.day, this.amount);
+}
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
 
-  List<Map<String, Object>> get groupedTransactionValues {
+  List<_Transaction> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index));
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
       double totalSum = 0;
+
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == recentTransactions[i].date.month &&
             recentTransactions[i].date.year == recentTransactions[i].date.year)
           totalSum += recentTransactions[i].amount;
       }
-      return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'ammount': totalSum};
+      return _Transaction(DateFormat.E().format(weekDay).substring(0, 1), totalSum);
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (previousValue, element) {
+      return previousValue + element.amount + 0;
     });
   }
 
@@ -26,10 +42,21 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((e) {
-          return Text("$e['day']");
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((e) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                e.day,
+                e.amount,
+                totalSpending == 0.0 ? 0.0 : e.amount / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
---
+++ expenses_app/lib/widgets/chart_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FittedBox(
+          child: Text('${spendingAmount.toStringAsFixed(0)}€'),
+        ),
+        SizedBox(
+          width: 4,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 5,
+          width: MediaQuery.of(context).size.width / 20,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 8),
+        Text(label),
+      ],
+    );
+  }
+}
