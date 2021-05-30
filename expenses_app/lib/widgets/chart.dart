caf0317bd5c38a8a5eb0251512bbca3a2ab16ffa
ajnart <thomas.camlong@epitech.eu>
2021-05-30 17:34:43 +0200

    Update chart backend components

---
+++ expenses_app/lib/widgets/chart.dart
@@ -0,0 +1,38 @@
+import 'package:expenses_app/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == recentTransactions[i].date.month &&
+            recentTransactions[i].date.year == recentTransactions[i].date.year)
+          totalSum += recentTransactions[i].amount;
+      }
+      return {'day': DateFormat.E(weekDay), 'ammount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[],
+          )
+        ],
+      ),
+    );
+  }
+}
