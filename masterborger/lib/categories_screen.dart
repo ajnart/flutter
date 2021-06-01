20bafe32578b4e4c4bb41d5d25550356f7d89863
ajnart <thomas.camlong@epitech.eu>
2021-06-01 12:38:12 +0200

    Rework UI

--- masterborger/lib/categories_screen.dart
+++ masterborger/lib/categories_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:masterborger/dummy_data.dart';
+
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 20,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 20,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(
+                color: e.color,
+                title: e.title,
+              ),
+            )
+            .toList(),
       ),
-      children: <Widget>[
-        Text('hello'),
-      ],
     );
   }
 }
