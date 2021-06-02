3f32cbd6023e838c43e0c03220412af992dd09c9
ajnart <thomas.camlong@epitech.eu>
2021-06-02 23:05:56 +0200

    Split the main screen into two separate screens

---
+++ masterborger/lib/screens/tabs_sceen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:masterborger/screens/categories_screen.dart';
+
+import 'favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
