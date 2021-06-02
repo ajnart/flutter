0088a9ff7ec7bed82b7a6e4d95b469ce855c8a66
ajnart <thomas.camlong@epitech.eu>
2021-06-02 22:11:45 +0200

    Redirect unknown urls to a basic page

--- masterborger/lib/main.dart
+++ masterborger/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:masterborger/screens/categories_screen.dart';
-import 'package:masterborger/screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,14 +20,20 @@ class MyApp extends StatelessWidget {
           textTheme: ThemeData.light().textTheme.copyWith(
               bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline6: TextStyle(
+              headline1: TextStyle(
                 fontFamily: 'RobotoCondensed',
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ))),
       home: CategoriesScreen(),
       routes: {
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
       },
     );
   }
