8ccf2c2fee1bbe228fa69c0cf8b6358a315a7176
ajnart <thomas.camlong@epitech.eu>
2021-06-03 00:55:47 +0200

    Added a list of switches to change the state of what's being filtered

--- masterborger/lib/screens/filters_screen.dart
+++ masterborger/lib/screens/filters_screen.dart
@@ -9,12 +9,69 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  bool _vegan = false;
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _lactoseFree = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      appBar: AppBar(
+          title: Text(
+        "Filter recipes that contain",
+        textAlign: TextAlign.center,
+      )),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: const Text('Lactose-free'),
+              value: _lactoseFree,
+              onChanged: (bool value) {
+                setState(() {
+                  _lactoseFree = value;
+                });
+              },
+              secondary: const Icon(Icons.lightbulb_outline),
+            ),
+            SwitchListTile(
+              title: const Text('Gluten-free'),
+              value: _glutenFree,
+              onChanged: (bool value) {
+                setState(() {
+                  _glutenFree = value;
+                });
+              },
+              secondary: const Icon(Icons.lightbulb_outline),
+            ),
+            SwitchListTile(
+              title: const Text('Vegan'),
+              value: _vegan,
+              onChanged: (bool value) {
+                setState(() {
+                  _vegan = value;
+                });
+              },
+              secondary: const Icon(Icons.lightbulb_outline),
+            ),
+            SwitchListTile(
+              title: const Text('Vegetarian'),
+              value: _vegetarian,
+              onChanged: (bool value) {
+                setState(() {
+                  _vegetarian = value;
+                });
+              },
+              secondary: const Icon(Icons.lightbulb_outline),
+            ),
+          ],
+        ),
       ),
     );
   }
