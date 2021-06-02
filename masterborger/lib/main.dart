9c211bdfc7cca6c89f747d931118b6874a212efc
ajnart <thomas.camlong@epitech.eu>
2021-06-02 22:57:45 +0200

    Tweak theme

--- masterborger/lib/main.dart
+++ masterborger/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
           textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText1: TextStyle(color: Color.fromRGBO(51, 51, 51, 1)),
               bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               headline1: TextStyle(
                 fontFamily: 'RobotoCondensed',
