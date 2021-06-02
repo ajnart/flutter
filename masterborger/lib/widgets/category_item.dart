fa426369b13a88aec6a2973420656407fc20eb12
ajnart <thomas.camlong@epitech.eu>
2021-06-02 23:05:17 +0200

    Update style for category item

--- masterborger/lib/widgets/category_item.dart
+++ masterborger/lib/widgets/category_item.dart
@@ -23,7 +23,7 @@ class CategoryItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(12),
-        child: Text(title, style: Theme.of(context).textTheme.headline6),
+        child: Center(child: Text(title, style: Theme.of(context).textTheme.headline6)),
         decoration: BoxDecoration(
           gradient: LinearGradient(colors: [
             color.withOpacity(0.7),
