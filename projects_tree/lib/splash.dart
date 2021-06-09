dfac128518594721ee1489145d170f824e271391
ajnart <thomas.camlong@epitech.eu>
2021-06-09 20:23:36 +0200

    Add a spashscreen

---
+++ projects_tree/lib/splash.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'linktree.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  VideoState createState() => VideoState();
+}
+
+class VideoState extends State<Splash> with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  startTime() async {
+    var _duration = Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => LinkTree()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    animation = CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/logo.png',
+                width: animation.value * 250,
+                height: animation.value * 250,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
