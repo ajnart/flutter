784c16ef27fbba18402e2e86e7473e70e7830c27
ajnart <thomas.camlong@epitech.eu>
2021-06-09 21:12:17 +0200

    Reorganise projects in the projects list

--- projects_tree/lib/data/projects.dart
+++ projects_tree/lib/data/projects.dart
@@ -7,6 +7,17 @@ List<Project> projects = [
       label: "Link Tree",
       description: "A Linktr.ee clone, it provides a simple onepage for all your social media links!"),
   Project(
+      url: '/flutter/managment_mockup/#/',
+      asset: 'assets/managment.png',
+      label: "Managment mockup",
+      description: "Experimenting with mockups in the form of a deshboard UI seen in a YouTube video about flutter"),
+  Project(
+    url: '/flutter/masterborger/#/',
+    description: "An application containing recipes for different types of food, sorted by categories.",
+    label: "Cooking ideas!",
+    asset: 'assets/masterborger.png',
+  ),
+  Project(
       url: '/flutter/forms/#/',
       asset: './assets/forms.png',
       label: "Forms",
@@ -17,16 +28,5 @@ List<Project> projects = [
     description: "A small app that tracks your weekly expenses.",
     label: "Expenses",
     asset: 'assets/expenses_app.png',
-  ),
-  Project(
-      url: '/flutter/managment_mockup/#/',
-      asset: 'assets/managment.png',
-      label: "Managment mockup",
-      description: "Experimenting with mockups in the form of a deshboard UI seen in a YouTube video about flutter"),
-  Project(
-    url: '/flutter/masterborger/#/',
-    description: "An application containing recipes for different types of food, sorted by categories.",
-    label: "Cooking ideas!",
-    asset: 'assets/masterborger.png',
   ),
 ];
