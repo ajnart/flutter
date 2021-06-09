092a4f86fe7059b3c2ff7903c4dba5623d9440a3
ajnart <thomas.camlong@epitech.eu>
2021-06-09 07:27:37 +0200

    Add register phone screen

---
+++ forms/lib/screens/register_phone_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterPhoneScreen extends StatelessWidget {
+  static const routeName = '/register-phone';
+  const RegisterPhoneScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
