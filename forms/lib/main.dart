f3dbe8d773cb0f11b4f0b40c970bc7106df8b950
ajnart <thomas.camlong@epitech.eu>
2021-06-09 07:27:30 +0200

    Update main screen

--- forms/lib/main.dart
+++ forms/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:forms/screens/register_phone_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,6 +35,9 @@ class MyApp extends StatelessWidget {
             ),
           )),
       home: MyHomePage(),
+      routes: {
+        RegisterPhoneScreen.routeName: (ctx) => RegisterPhoneScreen(),
+      },
     );
   }
 }
@@ -50,71 +54,55 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue[700],
-        toolbarHeight: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.white,
+        elevation: 2,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.api,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text('FORMS', style: Theme.of(context).textTheme.headline1),
+          ],
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            LogoWidget(),
-            Spacer(),
-            ElevatedButton(
-              style: ButtonStyle(elevation: MaterialStateProperty.all(2)),
-              onPressed: () {},
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 10),
-                child: Text("S'inscrire", style: TextStyle(fontSize: 20, color: Colors.white)),
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "J'ai déjà un compte",
-                style: TextStyle(
-                    fontSize: 17, letterSpacing: 0, color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Spacer(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LogoWidget extends StatelessWidget {
-  const LogoWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 15.0, bottom: 5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.api,
-                  color: Theme.of(context).primaryColor,
+        child: Center(
+          child: Column(
+            children: [
+              Spacer(),
+              ElevatedButton(
+                style: ButtonStyle(elevation: MaterialStateProperty.all(2)),
+                onPressed: () {
+                  Navigator.pushNamed(context, RegisterPhoneScreen.routeName);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 10),
+                  child: Text("S'inscrire", style: TextStyle(fontSize: 20, color: Colors.white)),
                 ),
-                SizedBox(
-                  width: 10,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "J'ai déjà un compte",
+                  style: TextStyle(
+                      fontSize: 17,
+                      letterSpacing: 0,
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold),
                 ),
-                Text('FORMS', style: Theme.of(context).textTheme.headline1),
-              ],
-            ),
+              ),
+              Spacer(),
+            ],
           ),
         ),
-        Divider(
-          thickness: 1,
-        ),
-      ],
+      ),
     );
   }
 }
