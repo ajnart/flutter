ee4445763f6d5c8f0f275698741bc574bc0ca828
ajnart <thomas.camlong@epitech.eu>
2021-06-09 04:42:03 +0200

    Change favorites screen styling

--- masterborger/lib/screens/favorites_screen.dart
+++ masterborger/lib/screens/favorites_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:masterborger/models/meal.dart';
+import 'package:masterborger/widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  final List<Meal> favouriteMeals;
-  const FavoritesScreen({Key? key, required this.favouriteMeals}) : super(key: key);
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - start adding some!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
