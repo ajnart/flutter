85375f7034e21425953651a906a90dc86ae3224d
ajnart <thomas.camlong@epitech.eu>
2021-06-10 14:37:52 +0200

    Implement ProductsOverviewScreen()

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/products_overview.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,24 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shop'),
-      ),
-      body: Center(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
