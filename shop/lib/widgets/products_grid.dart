39f81da258023727ebb5bb72d6a3eed210e2345b
ajnart <thomas.camlong@epitech.eu>
2021-06-12 20:30:22 +0200

    Change product overview

---
+++ shop/lib/widgets/products_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(12),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => Container(
+        child: ProductItem(products.elementAt(i)),
+      ),
+    );
+  }
+}
